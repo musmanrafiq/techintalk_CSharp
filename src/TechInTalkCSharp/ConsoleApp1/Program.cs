9baa49ef968cbb5b1d4bd16ca195f4dad169319a
Usman <usman.solemn@gmail.com>
2022-10-30 12:22:51 +0500

    Variables - 01

--- src/TechInTalkCSharp/ConsoleApp1/Program.cs
+++ src/TechInTalkCSharp/ConsoleApp1/Program.cs
@@ -2,9 +2,15 @@
 {
     internal class Program
     {
+        static int age = 30;
+        static string fullName = "Usman Rafiq";
+        static double salary = 100.5;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            age += 5;
+            Console.WriteLine($"Hello, { fullName}, and your age is: {age}, and salary is : {salary}");
         }
     }
 }
