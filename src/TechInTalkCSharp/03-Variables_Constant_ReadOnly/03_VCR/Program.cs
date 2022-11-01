49ac18a89f2de62c6d3c272b24598b2bee37b0ce
Usman <usman.solemn@gmail.com>
2022-11-01 12:00:42 +0500

    updates, Const and Readonly

---
+++ src/TechInTalkCSharp/03-Variables_Constant_ReadOnly/03_VCR/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03_VCR
+{
+    internal class Program
+    {
+        const int age = 10;
+
+
+        public Program()
+        {
+            //age = 11;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public void YourAge()
+        {
+           
+        }
+    }
+}
