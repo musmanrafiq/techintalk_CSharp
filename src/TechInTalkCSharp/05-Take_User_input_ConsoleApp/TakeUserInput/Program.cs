d72e15807893debb285d2a49316a1daf47f9c6c2
Usman <usman.solemn@gmail.com>
2022-11-04 11:56:20 +0500

    05-Take_User_input_ConsoleApp updates

---
+++ src/TechInTalkCSharp/05-Take_User_input_ConsoleApp/TakeUserInput/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TakeUserInput
+{
+    // There are two types of input in console application
+    // 1- when we start a program
+    // 2- while running an application, get input from console
+    //    2.1 - Read char from user input
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine($"Hello, World! {args[2]}");
+
+            Console.WriteLine("Please enter you name");
+
+            string userInput = Console.ReadLine();
+
+            
+
+            Console.WriteLine($"Welcome to c# tutorial series {userInput}");
+            
+            ConsoleKeyInfo userInputChar = Console.ReadKey();
+            Console.WriteLine(userInputChar.KeyChar);
+            
+        }
+    }
+}
