822769e49f59d3bf1c86987d5dadcf225126f6bc
Usman <usman.solemn@gmail.com>
2022-12-22 13:08:06 +0500

    Enums 15

---
+++ src/TechInTalkCSharp/15-Emums/15-Enums/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _15_Enums
+{
+    public enum WeekDays{
+        Monday = 2,
+        Tuesday = 4,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int todaysValue = WeekDays.Monday;
+
+            foreach(var tempValue in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine($"Today is {(int)tempValue}");
+            }
+            
+        }
+    }
+}
