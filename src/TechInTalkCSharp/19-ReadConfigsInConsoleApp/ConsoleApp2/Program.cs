fba3788394022295f3bdb9d409eb62a8d9b5255d
Usman <usman.solemn@gmail.com>
2023-02-01 19:28:11 +0500

    Read configurations from appConfig.json

--- src/TechInTalkCSharp/19-ReadConfigsInConsoleApp/ConsoleApp2/Program.cs
+++ src/TechInTalkCSharp/19-ReadConfigsInConsoleApp/ConsoleApp2/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace ConsoleApp2
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var binder = new ConfigurationBuilder().
+                SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appConfig.json");
+
+            IConfiguration configuration = binder.Build();
+            var emailSetting = configuration.GetSection("EmailService").Get<EmailServiceConfig>();
+
+            Console.WriteLine($"Email is {emailSetting.Email}, smtp is {emailSetting.Smtp} and the password is" +
+                $" {emailSetting.Password}");
         }
+    }
+
+    public class EmailServiceConfig
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Smtp { get; set; }
     }
 }
