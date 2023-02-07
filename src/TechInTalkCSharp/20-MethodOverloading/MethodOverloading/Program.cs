20116db76e9aed715e1ed175db1790ff76127d40
Usman <usman.solemn@gmail.com>
2023-02-07 20:03:50 +0500

    20, Method overloading

---
+++ src/TechInTalkCSharp/20-MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MethodOverloading
+{
+    // what is method overloading
+    // why is  that useful
+    // how we will implement it
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSystem fs = new FileSystem();
+            //fs.Create()
+        }
+    }
+
+    public class FileSystem
+    {
+        public void Create()
+        {
+
+        }
+        public void Create(string fileName)
+        {
+
+        }
+        public string Create(int fileName)
+        {
+            return string.Empty;
+        }
+        public void Create(string fileName, string directory)
+        {
+
+        }
+        public string Create(string fileName, int priority)
+        {
+            return string.Empty;
+        }
+        public string Create(string fileName, int priority, string permissions)
+        {
+            return string.Empty;
+        }
+    }
+}
