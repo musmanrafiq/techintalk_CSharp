2dca3f9c29b02bbdb41aa4a1543ff11ce95fec68
Usman <usman.solemn@gmail.com>
2023-02-08 19:55:16 +0500

    21 DI console application

---
+++ src/TechInTalkCSharp/21-DIConsoleApp/DIConsoleApp/EmailService.cs
@@ -0,0 +1,15 @@
+﻿
+namespace DIConsoleApp
+{
+    public interface IEmailService
+    {
+        void SendEmail(string email);
+    }
+    internal class EmailService : IEmailService
+    {
+        public void SendEmail(string email)
+        {
+            // implementation
+        }
+    }
+}
---
+++ src/TechInTalkCSharp/21-DIConsoleApp/DIConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DIConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // all services configurations
+            ServiceProvider services = ServicesDI();
+            // game object 
+            var gameObject = services.GetService<Game>();
+            // game started
+            gameObject.Start();
+
+        }
+
+        private static ServiceProvider ServicesDI()
+        {
+            var servicesCollection = new ServiceCollection();
+            // lets list / add all the dependencies here
+            servicesCollection.AddTransient<IEmailService, EmailService>();
+            servicesCollection.AddSingleton<Game>();
+            //servicesCollection.AddSingleton(typeof(Game));
+            var services = servicesCollection.BuildServiceProvider();
+            return services;
+        }
+    }
+}
