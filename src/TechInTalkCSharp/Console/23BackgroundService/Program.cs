66aa98e613c2e2e9b197a02a7b3e644d6a408a43
Usman <usman.solemn@gmail.com>
2023-02-22 11:46:09 +0500

    updates, Background Service console Application.

---
+++ src/TechInTalkCSharp/Console/23BackgroundService/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace _23BackgroundService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureHostConfiguration(hconfig => { })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddHostedService<MyProcessor>();
+                }).UseConsoleLifetime().Build();
+            host.Run();
+        }
+    }
+
+    public class MyProcessor : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Console.WriteLine($"The current date is {DateTime.Now.Date}");
+                await Task.Delay(1000);
+            }
+            return;
+        }
+    }
+}
