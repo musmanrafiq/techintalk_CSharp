87edaf6e42d2c00030a5d7fcb72d594e8132f7bc
Usman <usman.solemn@gmail.com>
2023-03-17 19:45:41 +0500

    Params keyword in c#

---
+++ src/TechInTalkCSharp/General/ParamsKeyword/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ParamsKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Bio("Usman","Rafiq");
+        }
+
+        public class Person
+        {
+            public void Bio(params string[] information)
+            {
+                Console.WriteLine(information[0] + " " + information[1]);
+            }
+        }
+    }
+}
