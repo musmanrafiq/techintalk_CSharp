f076f331f535fa5b5f67b97dbb5e12cfff464e2f
Usman <usman.solemn@gmail.com>
2023-03-17 20:17:59 +0500

    updates

--- src/TechInTalkCSharp/General/Conversions/Program.cs
+++ src/TechInTalkCSharp/General/Conversions/Program.cs
@@ -4,6 +4,21 @@
     {
         static void Main(string[] args)
         {
+            string? amount = "1000";
+            int convertedValue;
+            // by using int.parse
+            //int firstConversion = Int32.Parse(amount);
+
+            // by using convert.ToInt32
+            //int secondConversion = Convert.ToInt32(amount);
+
+            //Console.WriteLine(secondConversion);
+
+            // by using int.parse
+            int firstConversion = Int32.Parse(amount);
+
+            // by using int.TryParse
+            bool withTryParse = int.TryParse(amount, out convertedValue);
         }
     }
 }
