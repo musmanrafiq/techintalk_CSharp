16351de07c40b0ea5521ec35b2cdc65ae1178558
Usman <usman.solemn@gmail.com>
2023-03-17 19:51:54 +0500

    updates

---
+++ src/TechInTalkCSharp/General/Conversions/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Conversions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- src/TechInTalkCSharp/General/ParamsKeyword/Program.cs
+++ src/TechInTalkCSharp/General/ParamsKeyword/Program.cs
@@ -2,15 +2,19 @@
 {
     internal class Program
     {
+        // the params keyword will only be used with array
+        // params type of argument should be last in the list or argument
+        // We cannot pass default value
+
         static void Main(string[] args)
         {
             Person p = new Person();
-            p.Bio("Usman","Rafiq");
+            p.Bio(1,2,3,4,5,6,7,8);
         }
 
         public class Person
         {
-            public void Bio(params string[] information)
+            public void Bio(params int[] information)
             {
                 Console.WriteLine(information[0] + " " + information[1]);
             }
