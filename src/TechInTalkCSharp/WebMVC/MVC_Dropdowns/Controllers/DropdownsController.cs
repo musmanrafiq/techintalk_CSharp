6097dcbf9ad6f705fd69a8b46d70dddca98afaaa
Usman <usman.solemn@gmail.com>
2023-03-21 11:07:51 +0500

    Updates

---
+++ src/TechInTalkCSharp/WebMVC/MVC_Dropdowns/Controllers/DropdownsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Dropdowns.Controllers
+{
+    public class DropdownsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
