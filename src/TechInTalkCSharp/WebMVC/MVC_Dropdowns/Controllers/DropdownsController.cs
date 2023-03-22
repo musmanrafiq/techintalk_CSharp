35a9e71dfb64c5caa0377b98db8ef702b08aa85c
Usman <usman.solemn@gmail.com>
2023-03-22 11:38:11 +0500

    Asp.net mvc dropdownlist

--- src/TechInTalkCSharp/WebMVC/MVC_Dropdowns/Controllers/DropdownsController.cs
+++ src/TechInTalkCSharp/WebMVC/MVC_Dropdowns/Controllers/DropdownsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MVC_Controls.Controllers
 {
@@ -11,6 +12,20 @@ namespace MVC_Controls.Controllers
 
         public IActionResult InlineInView()
         {
+            return View();
+        }
+
+        public IActionResult ControlerToView()
+        {
+            var list = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Pakistan", Value="1"},
+                new SelectListItem { Text = "United Kingdom", Value="2"},
+                new SelectListItem { Text = "USA", Value="3"}
+            };
+
+            ViewBag.Countries = list;
+            ViewData["ListOfCountries"] = list;
             return View();
         }
     }
