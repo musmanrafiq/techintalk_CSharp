42d66287b3dfd88c5b7f0966528d204b151c5381
Usman <usman.solemn@gmail.com>
2023-04-02 12:47:36 +0500

    Local Function and Static Local Function

---
+++ src/TechInTalkCSharp/General/LanguageFeatures/LocalFunction.cs
@@ -0,0 +1,30 @@
+﻿namespace LanguageFeatures
+{
+    public class LocalFunction
+    {
+        public void Example()
+        {
+            var list1 = new List<string>()
+            {
+                "Emp1", "Emp2", "Emp3"
+            };
+            var list2 = new List<string>()
+            {
+                "Emp4", "Emp5", "Emp6"
+            };
+
+            PrintList(list1);
+            PrintList(list2);
+
+            static void PrintList(List<string> list)
+            {
+                //list2.Add("Invalid addition");
+                foreach (var item in list)
+                {
+                    var newItem = item.Replace("Emp", string.Empty);
+                    Console.WriteLine(newItem);
+                }
+            }
+        }
+    }
+}
---
+++ src/TechInTalkCSharp/General/LanguageFeatures/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LanguageFeatures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var localFunction = new LocalFunction();
+            localFunction.Example();
+        }
+    }
+}
