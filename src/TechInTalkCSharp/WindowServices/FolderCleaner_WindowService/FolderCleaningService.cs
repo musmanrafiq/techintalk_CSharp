e28752b0a7d8d03f0490c3ece2059b295637a174
Usman <usman.solemn@gmail.com>
2023-09-29 15:34:27 +0500

    Folder Cleaning Service

---
+++ src/TechInTalkCSharp/WindowServices/FolderCleaner_WindowService/FolderCleaningService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace FolderCleaner_WindowService
+{
+    public sealed class FolderCleaningService : BackgroundService
+    {
+        private readonly ILogger<FolderCleaningService> _logger;
+
+        public FolderCleaningService(
+            ILogger<FolderCleaningService> logger) =>
+            (_logger) = (logger);
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    _logger.LogWarning($"Warning {DateTime.Now}");
+                    _logger.LogDebug($"Debug {DateTime.Now}");
+                    _logger.LogTrace($"Trace {DateTime.Now}");
+                    _logger.LogCritical($"Critical {DateTime.Now}");
+                    _logger.LogError($"Error {DateTime.Now}");
+
+
+                    _logger.LogInformation($"Deletion started at {DateTime.Now}");
+
+
+                    DirectoryInfo di = new DirectoryInfo("F:\\Development\\temp\\windowservice");
+
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        file.Delete();
+                    }
+                    foreach (DirectoryInfo dir in di.GetDirectories())
+                    {
+                        dir.Delete(true);
+                    }
+
+                    _logger.LogInformation($"Deletion ended at {DateTime.Now}");
+
+                    await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+
+                }
+            }
+            catch (TaskCanceledException)
+            {
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "{Message}", ex.Message);
+                Environment.Exit(1);
+            }
+        }
+    }
+}
---
+++ src/TechInTalkCSharp/WindowServices/FolderCleaner_WindowService/Program.cs
@@ -0,0 +1,26 @@
+﻿using FolderCleaner_WindowService;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddWindowsService(options =>
+        {
+            options.ServiceName = "Folder Cleaning Service";
+        });
+
+        LoggerProviderOptions.RegisterProviderOptions<
+            EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
+        builder.Services.AddHostedService<FolderCleaningService>();
+
+        IHost host = builder.Build();
+        host.Run();
+
+    }
+}
