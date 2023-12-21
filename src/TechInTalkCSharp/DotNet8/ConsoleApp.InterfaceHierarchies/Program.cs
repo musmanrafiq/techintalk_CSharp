7c913299e6f5ad08f330e3ac178386d33cd572cf
Usman <usman.solemn@gmail.com>
2023-12-21 18:58:17 +0500

    updates

---
+++ src/TechInTalkCSharp/DotNet8/ConsoleApp.InterfaceHierarchies/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+
+namespace ConsoleApp.InterfaceHierarchies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Drived impement object
+            IDerived value = new DerivedImplement { Base = 0, Derived = 1 };
+            // serializing the object
+            var serializedObject = JsonSerializer.Serialize(value);
+            // output on the console
+            Console.WriteLine(serializedObject);
+        }
+    }
+
+    public interface IBase
+    {
+        public int Base { get; set; }
+    }
+
+    public interface IDerived : IBase
+    {
+        public int Derived { get; set; }
+    }
+
+    public class DerivedImplement : IDerived
+    {
+        public int Base { get; set; }
+        public int Derived { get; set; }
+    }
+}
---
+++ src/TechInTalkCSharp/EFCore/DotNet8_CRUD/WebApi/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55)
+            })
+            .ToArray();
+        }
+    }
+}
