37b884fb8b92a8ec45ac23aaf23e5efa038c1921
Usman <usman.solemn@gmail.com>
2024-02-26 20:05:29 +0500

    updates

--- src/TechInTalkCSharp/FeatureFlags/Dotnet_Own/DotnetFeatureFlag_ConsoleApp/Program.cs
+++ src/TechInTalkCSharp/FeatureFlags/Dotnet_Own/DotnetFeatureFlag_ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DotnetFeatureFlag_ConsoleApp
 {
@@ -6,19 +7,31 @@ namespace DotnetFeatureFlag_ConsoleApp
     {
         static void Main(string[] args)
         {
-
-
+            // builder object with configurations loaded from json file
             var builder = new ConfigurationBuilder().AddJsonFile("appSettings.json", false, true);
-
+            // building configs
             var config = builder.Build();
 
+            // First video
             var testKey = config["TestKey"];
+            Console.WriteLine(testKey);
 
-            Console.WriteLine($"Hello, World! {testKey}");
+            //
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<TestClass>()
+                .AddSingleton<IConfiguration>(config)
+                .BuildServiceProvider();
 
-            Console.ReadKey();
+            // Second Video
+            var testValueFromObj = config["Test:Key"];
+            var testValueFromObjUsingGetSection = config.GetSection("Test:Key");
+            Console.WriteLine(testValueFromObj);
+            Console.WriteLine(testValueFromObjUsingGetSection.Value);
 
-            Console.WriteLine($"Hello, World! {testKey}");
+            TestClass testClassObj = serviceProvider.GetService<TestClass>();
+
+            testClassObj?.PrintConfig();
+
         }
     }
 }
---
+++ src/TechInTalkCSharp/FeatureFlags/Dotnet_Own/DotnetFeatureFlag_ConsoleApp/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DotnetFeatureFlag_ConsoleApp
+{
+    internal class TestClass
+    {
+        // config interface
+        private readonly IConfiguration configuration;
+
+        // injection of config
+        public TestClass(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void PrintConfig()
+        {
+            Console.WriteLine($"{this.configuration["Test:Key"]} is coming from Test Class");
+        }
+    }
+}
