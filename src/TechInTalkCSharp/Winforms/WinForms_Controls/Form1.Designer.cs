d89eede0e7ffd7041163ce9b73094591fceee7de
Usman <usman.solemn@gmail.com>
2024-03-16 10:58:22 +0500

    updates

--- src/TechInTalkCSharp/Winforms/WinForms_Controls/Form1.Designer.cs
+++ src/TechInTalkCSharp/Winforms/WinForms_Controls/Form1.Designer.cs
@@ -32,21 +32,24 @@
             login_btn = new Button();
             userEmail_lbl = new Label();
             clickResult_lbl = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
             SuspendLayout();
             // 
             // email_txtbox
             // 
             email_txtbox.Location = new Point(137, 23);
             email_txtbox.Name = "email_txtbox";
-            email_txtbox.Size = new Size(375, 27);
+            email_txtbox.PlaceholderText = "Your email address";
+            email_txtbox.Size = new Size(504, 27);
             email_txtbox.TabIndex = 0;
             // 
             // login_btn
             // 
-            login_btn.Location = new Point(137, 66);
+            login_btn.Location = new Point(137, 121);
             login_btn.Name = "login_btn";
             login_btn.Size = new Size(73, 29);
-            login_btn.TabIndex = 1;
+            login_btn.TabIndex = 3;
             login_btn.Text = "Login";
             login_btn.UseVisualStyleBackColor = true;
             login_btn.Click += login_click;
@@ -63,22 +66,44 @@
             // clickResult_lbl
             // 
             clickResult_lbl.AutoSize = true;
-            clickResult_lbl.Location = new Point(236, 75);
+            clickResult_lbl.Location = new Point(234, 130);
             clickResult_lbl.Name = "clickResult_lbl";
             clickResult_lbl.Size = new Size(0, 20);
             clickResult_lbl.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(137, 68);
+            textBox1.Name = "textBox1";
+            textBox1.PasswordChar = '*';
+            textBox1.PlaceholderText = "Your password";
+            textBox1.Size = new Size(504, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(37, 75);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 20);
+            label1.TabIndex = 5;
+            label1.Text = "Password";
+            label1.Click += label1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(653, 207);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
             Controls.Add(clickResult_lbl);
             Controls.Add(userEmail_lbl);
             Controls.Add(login_btn);
             Controls.Add(email_txtbox);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Login";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -89,5 +114,7 @@
         private Button login_btn;
         private Label userEmail_lbl;
         private Label clickResult_lbl;
+        private TextBox textBox1;
+        private Label label1;
     }
 }
--- src/TechInTalkCSharp/Winforms/WinForms_Controls/Form1.cs
+++ src/TechInTalkCSharp/Winforms/WinForms_Controls/Form1.cs
@@ -12,5 +12,16 @@ namespace WinForms_Controls
             var userEmail = this.email_txtbox.Text.ToString();
             clickResult_lbl.Text = userEmail;
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var capsLockStatus = IsKeyLocked(Keys.CapsLock);
+            clickResult_lbl.Text = capsLockStatus ? "Caps On" : "";
+        }
     }
 }
