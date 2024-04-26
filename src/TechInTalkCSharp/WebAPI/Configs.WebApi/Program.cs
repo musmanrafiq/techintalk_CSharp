a95c521915989e19ff6bf37ce115b8e3322563b3
Usman <usman.solemn@gmail.com>
2024-04-26 20:04:21 +0500

    Minimal API read config using ICofigurations interface

---
+++ src/TechInTalkCSharp/WebAPI/Configs.WebApi/Program.cs
@@ -0,0 +1,34 @@
+namespace Configs.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            //app.UseHttpsRedirection();
+            //app.UseAuthorization();
+
+
+            app.MapGet("/weatherforecast", (HttpContext httpContext, IConfiguration config) =>
+            {
+
+                var userSettings = config.GetSection("Users").Get<UserSetting>();
+                return userSettings;
+            });
+
+            app.Run();
+        }
+    }
+
+    public record UserSetting
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
